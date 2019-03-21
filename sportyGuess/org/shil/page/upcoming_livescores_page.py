c904ca12c41f5ae9ea5d2347466fc6b90b14e37b
vanis <vanis@OKOK>
2019-03-22 00:48:26 +0800

    better upcoming link anaylze

--- sportyGuess/org/shil/page/upcoming_livescores_page.py
+++ sportyGuess/org/shil/page/upcoming_livescores_page.py
@@ -20,23 +20,30 @@ try:
     for tr in trs:
         try:
             id = tr.get_attribute('id')
+            print('process id:'+id)
             if(id.find('g')!= -1) :
-    #             print("g : "+id)
                 a = tr.find_elements_by_tag_name('a')[0]
                 tournament_link = a.get_attribute('href')
                 tournament_name = a.text
                 params = [tournament_link,tournament_name]
                 tournament_maps[tournament_name] = tournament_link
-    #             fetch_url_repository.insert_fetch_url(tournament_link, fetch_url_repository.type_Tournament, params, fetch_url_repository.priority_High)
-    #             print(tournament_name)
-    #             print(tournament_link)
             elif(id.find('i') != -1 ):
-    #             print('i : '+id)
                 tds = tr.find_elements_by_tag_name('td')
                 try:
-                    preview = tds[9].find_elements_by_tag_name('a')[0].text
-                    if(utils.getStr(preview) == 'Stream'):
-                        print('Found Steam here , Ignore .')
+#                     print(tds[9].text)
+                    if len(tds[9].text) < 1:
+                        print('1. Nothing here bye')
+                        continue
+                    a = tds[9].find_elements_by_tag_name('a')
+#                     print(len(a))
+                    if(len(a)<2):
+                        print('2. no link here')
+                        continue
+                    preview = a[0].text
+                    print(preview)
+                    if(utils.getStr(preview) != 'Preview' 
+                       and utils.getStr(preview) !='Match Report'):
+                        print('3. Not Found Preview or Match Report here , Ignore .')
                         continue
                 except Exception as e:
                     print('Found Exception here , Ignore .')
@@ -44,42 +51,37 @@ try:
                     continue
                 home_team_link = tds[5].find_element_by_tag_name('a').get_attribute('href')
                 team_links.append(home_team_link)
-    #             fetch_url_repository.insert_fetch_url(home_team_link, fetch_url_repository.type_TeamHome, home_team_link, fetch_url_repository.priority_High)
-    #             print(home_team_link)
                 match_id = utils.find_match_id_from_matchurl(tds[6].find_elements_by_tag_name('a')[0].get_attribute('href'))
-    #             print(match_id)
                 match_ids.append(match_id)
                 away_team_link = tds[7].find_element_by_tag_name('a').get_attribute('href')
                 team_links.append(away_team_link)
-    #             fetch_url_repository.insert_fetch_url(away_team_link, fetch_url_repository.type_TeamHome, away_team_link, fetch_url_repository.priority_High)
-    #             print(away_team_link)
             else:
                 print('NNNNNNNNNNNNNNNNNNNNNNNNNever come here!')
                 break
             
-        except Exception as e:
-            print("e , bye")
+        except Exception:
+            print("tr, e , bye")
             pass
         
     print('start insert tournaments:')
     for key in tournament_maps.keys() :
         print(key+" : "+ tournament_maps[key])
         params = [tournament_maps[key],key]
-        if(fetch_url_repository.update_url_priority(tournament_maps[key], fetch_url_repository.priority_High) < 0 ):
-            fetch_url_repository.insert_fetch_url(tournament_maps[key], fetch_url_repository.type_Tournament, params,fetch_url_repository.priority_High)
+#         if(fetch_url_repository.update_url_priority(tournament_maps[key], fetch_url_repository.priority_High) < 0 ):
+#             fetch_url_repository.insert_fetch_url(tournament_maps[key], fetch_url_repository.type_Tournament, params,fetch_url_repository.priority_High)
     
     print('start insert teams link:')
     for team_link in team_links:
         print(team_link)
-        if(fetch_url_repository.update_url_priority(team_link, fetch_url_repository.priority_High) < 0 ):
-            fetch_url_repository.insert_fetch_url(team_link, fetch_url_repository.type_TeamHome, team_link, fetch_url_repository.priority_High)
+#         if(fetch_url_repository.update_url_priority(team_link, fetch_url_repository.priority_High) < 0 ):
+#             fetch_url_repository.insert_fetch_url(team_link, fetch_url_repository.type_TeamHome, team_link, fetch_url_repository.priority_High)
         
     print('start insert matches :')
     for match_id in match_ids:
         print(match_id)
         preview_match_url = 'https://www.whoscored.com/Matches/'+match_id+'/Preview'
-        if(fetch_url_repository.update_url_priority(preview_match_url, fetch_url_repository.priority_High) < 0 ):
-            fetch_url_repository.insert_fetch_url(preview_match_url, fetch_url_repository.type_MatchPreview, preview_match_url, fetch_url_repository.priority_High)
+#         if(fetch_url_repository.update_url_priority(preview_match_url, fetch_url_repository.priority_High) < 0 ):
+#             fetch_url_repository.insert_fetch_url(preview_match_url, fetch_url_repository.type_MatchPreview, preview_match_url, fetch_url_repository.priority_High)
 
 finally:
     browser.quit()
